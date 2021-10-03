07a26675935cc8c9527c4818a67f7f32d30d177a
Jon Eccles <getjon@me.com>
2021-10-03 19:36:28 +0000

    Add the ability to view a list of Bookings

---
+++ spec/factories/bookings.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :booking do
+    name { "Regular Customer" }
+  end
+end
---
+++ spec/features/viewing_bookings_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.feature "Users can view bookings" do  
+  scenario "with the booking details" do
+    booking = FactoryBot.create(:booking, name: "Regular Customer")
+    visit "/"
+    click_link "Regular Customer"  
+    expect(page.current_url).to eq booking_url(booking)
+  end
+end
