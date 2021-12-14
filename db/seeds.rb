1bbb71d002c5d5f5ad301dc1bd24438023cbc007
Jon Eccles <getjon@me.com>
2021-12-14 18:15:09 +0000

    Booking and pet counts on customer index. DB seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+customer = Customer.create(
+  {
+    name: 'Jon Eccles',
+    phone: '07791 513121',
+    email: 'getjon@me.com',
+    notes:
+    %(Jon likes to leave his rabbits here for a long time and has failed to pay on a couple of occaisions resulting in a lot of chasing!).squish,
+    vet_details:
+    %(Gayton Surgery, Hatchlands Road, Reigate).squish
+  }
+)
+customer.bookings.create(
+  [{
+    drop_off: '01/08/2021',
+    pick_up: '26/08/2021',
+    notes: 'Dropping off in the morning, picking up mid-afternoon'
+  },
+   {
+     drop_off: '21/10/2021',
+     pick_up: '30/10/2021',
+     notes: ''
+   }, {
+     drop_off: '19/12/2021',
+     pick_up: '03/01/2022',
+     notes: 'Will drop off Xmas presents for the pets'
+   }, {
+     drop_off: '10/02/2022',
+     pick_up: '26/02/2022',
+     notes: ''
+   }]
+)
+customer.pets.create(
+  [{
+    name: 'Thumper',
+    animal: 'House Rabbit',
+    vaccinated: true,
+    status: 'Active',
+    notes: 'Feisty - watch yourself when handling!'
+  },
+   {
+     name: 'Humphrey',
+     animal: 'Guinea Pig',
+     vaccinated: false,
+     status: 'Active',
+     notes: "Doesn't say much"
+   }]
+)
