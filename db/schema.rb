72dc4fceea256f84a16a33cf1178e5e881b33c96
Jon Eccles <getjon@me.com>
2021-12-29 14:49:10 +0000

    Added Devise and Sign up feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_064101) do
+ActiveRecord::Schema.define(version: 2021_12_29_140112) do
 
   create_table "bookings", force: :cascade do |t|
     t.date "drop_off"
@@ -43,6 +43,18 @@ ActiveRecord::Schema.define(version: 2021_12_15_064101) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["customer_id"], name: "index_pets_on_customer_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "bookings", "customers"
